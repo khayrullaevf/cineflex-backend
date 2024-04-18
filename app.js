069abc97bd84657d14ce6539f9733f460cbbc19a
khayrullaevf <fazliddinkhayrullaev4@gmail.com>
2024-04-18 09:48:33 +0300

    movies api

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 
 const express=require('express')
 let app=express()
+const fs=require('fs')
+let movies=JSON.parse(fs.readFileSync('./data/movies.json'))
 
 
 //ROUTE=HTTP METHOD+ URL
-app.get('/',(req,res)=>{
-    res.status(200).json({message:'Hello, world', status:200})
-})
 
-app.post('/',()=>{
+//GET-api/movies
 
+app.get('/api/v1/movies',(req,res)=>{
+    res.status(200).json({
+        status:"success",
+        count:movies.length,
+        data:{ 
+            movies:movies
+        }})
 })
 
 
 //CREATE A SERVER
-const port=8000
+const port=3000
 app.listen(port , ()=>{
     console.log('Server has started');
 })
