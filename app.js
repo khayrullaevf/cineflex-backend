17f248505a6dea5cf68e9b531c035f5d283b9bd2
khayrullaevf <fazliddinkhayrullaev4@gmail.com>
2024-04-18 13:27:28 +0300

    update movie

--- app.js
+++ app.js
@@ -36,6 +36,57 @@ app.post('/api/v1/movies',(req,res)=>{
 
 })
 
+//GET= api/v1/movies/:id
+app.get('/api/v1/movies/:id',(req,res)=>{
+    const id=req.params.id*1
+    let movie= movies.find((movie)=>movie.id===id)
+
+
+   if(!movie){
+    return  res.status(400).json({
+        status:"fail",
+        message:'Movie with id '+id+' not found'
+      })
+   }
+
+    res.status(200).json({
+        status:"success",
+        data:{
+            movie:movie
+        }
+    })
+
+})
+
+//PATCH -update release year
+app.patch('/api/v1/movies/:id',(req,res)=>{
+   let id=req.params.id*1;
+   let movieToUpdate=movies.find((movie)=>movie.id===id)
+   if (!movieToUpdate) {
+    return  res.status(400).json({
+        status:"fail",
+        message:'No Movie with id '+id+' not found'
+      })
+   }
+
+
+
+
+   let movieIndex=movies.indexOf(movieToUpdate)
+   Object.assign(movieToUpdate,req.body)
+   movies[movieIndex]=movieToUpdate
+
+   fs.writeFile("./data/movies.json",JSON.stringify(movies),(err)=>{
+    res.status(200).json({
+        status:"success",
+        data:{
+            movie:movieToUpdate
+        }
+    })
+
+   })
+
+})
 
 //CREATE A SERVER
 const port=3000
