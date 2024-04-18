200f603aeb8f01a2670731e21ad2dc836fa4c453
khayrullaevf <fazliddinkhayrullaev4@gmail.com>
2024-04-18 08:25:26 +0300

    get , post

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ let app=express()
 
 
 //ROUTE=HTTP METHOD+ URL
+app.get('/',(req,res)=>{
+    res.status(200).json({message:'Hello, world', status:200})
+})
+
+app.post('/',()=>{
+
+})
 
 
 //CREATE A SERVER
