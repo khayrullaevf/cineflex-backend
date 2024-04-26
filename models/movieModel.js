4b71cff3997b7296cf74185dc07841f5efa34b0f
khayrullaevf <fazliddinkhayrullaev4@gmail.com>
2024-04-26 08:52:03 +0300

    data validators

--- models/movieModel.js
+++ models/movieModel.js
@@ -1,13 +1,17 @@
 const mongoose=require('mongoose')
 
 const fs=require('fs')
+const validator=require('validator')
 
 const movieSchema=new mongoose.Schema({
     name:{
         type:String,
         required:[true,'Name is required'],
         unique:true,
-        trim:true
+        trim:true,
+        maxlength:[100,'Movie name must not have more than 100 characters'],
+        minlength:[4,'Movie name must have at least 4 characters'],
+        validate:[validator.isAlpha,'Name show only contain alphabets(a-z,A-Z).']
     },
     description:{
         type:String,
@@ -19,7 +23,9 @@ const movieSchema=new mongoose.Schema({
     },
     ratings:{
         type:Number,
-        default:1.0
+        default:1.0,
+        min:[1,'Ratings must be 1 or greater than 1'],
+        max:[10,'Ratings must be 10 or less than 10']
     },
     totalRatings:{
         type:Number
@@ -40,7 +46,10 @@ const movieSchema=new mongoose.Schema({
     },
     genres:{
         type:[String],
-        required:[true,'Genres are required']
+        required:[true,'Genres are required'],
+        enum:{values:["Action", "Adventure", "Sci-Fi", "Thriller", "Crime", "Drama", "Comedy", "Romance", "Biography"],
+        message:'This genre does not exist'
+    }
      },
      directors:{
          type:[String],
