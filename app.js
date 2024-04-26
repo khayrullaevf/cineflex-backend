e37a5a7b961b0b1c729ea4bb0671e445cfadc5b3
khayrullaevf <fazliddinkhayrullaev4@gmail.com>
2024-04-26 10:19:50 +0300

    global error handling

--- app.js
+++ app.js
@@ -31,5 +31,34 @@ app.use((req,res,next)=>{
 //ROUTE
 app.use('/api/v1/movies',moviesRouter)
 
+app.all('*',(req,res,next)=>{
+    // res.status(400).json({
+    //     status:"fail",
+    //     message:`Can't find ${req.originalUrl} on the server!`
+    // })
+    const err= new Error(`Can't find ${req.originalUrl} on the server!`)
+    err.status="fail"
+    err.statusCode=404
+    next(err)
+})
+
+app.use((error,req,res,next)=>{
+
+    error.statusCode=error.statusCode||500
+    error.status=error.status||'error'
+
+    res.status(error.statusCode).json({
+        status:error.status,
+        message:error.message
+    })
+
+})
+
+
+
+
+
+
+
 module.exports=app
 
